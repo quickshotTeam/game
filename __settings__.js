614c035946a11e67c343b272ac1c5593f3a76bb7
quickshotTeam <128719061+quickshotTeam@users.noreply.github.com>
2023-03-23 19:18:53 +0300

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1679994.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 124099938, 124104374, 124106011, 124106022 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/124099928/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/124099929/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/124099927/1/ammo.js', 'preload' : true},
+];
